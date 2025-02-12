8143f592f2758cffc447915d9953800799dae8ab
jhonatas flor <jhonatas.flor@dcx.ufpb.br>
2025-02-12 14:10:58 -0300

    Cards

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import Button from "./components/Button";
 import CardsSection from "./components/CardsSection";
 import Card from "./components/Card";
 import ContainerCards from "./components/ContainerCards";
 import ContainerMap from "./components/ContainerMap";
 import InformationContainer from "./components/InformationContainer";
 import Map from "./components/Map";
+import { useState } from "react";
 
 import "leaflet/dist/leaflet.css";
 
-
 function App() {
+  const [cards, setCards] = useState([]);
+  const [cardId, setCardId] = useState(1); // ID autoincrementado
+
+  function newCard() {
+    setCards([...cards, { id: cardId, content: `Card ${cardId}` }]);
+    setCardId(cardId + 1);
+  }
+
+  function removeCard(id) {
+    setCards(cards.filter((card) => card.id !== id)); // Remove o card com o ID especificado
+  }
+
   return (
     <>
       <div>        
-          <CardsSection>
-            <ContainerCards>
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </ContainerCards>
-            <Button 
-              props={{text : "Enviar Coordenadas", }}
-            />
-            <Button 
-              props={{text : "Novas Coordenadas", }}
-            />
-          </CardsSection>
+        <CardsSection>
+          <ContainerCards>
+            {cards.map((card) => (
+              <Card key={card.id} id={card.id} onRemove={removeCard}>
+                {card.content}
+              </Card>
+            ))}
+          </ContainerCards> 
+          <button className={"actionBtn"} onClick={newCard}> Novas Coordenadas </button>
+          <button className={"actionBtn"}> Enviar Coordenadas </button>
+        </CardsSection>
 
-          <ContainerMap>
-            <InformationContainer/>
-            <Map />
-          </ContainerMap>
-          
+        <ContainerMap>
+          <InformationContainer/>
+          <Map />
+        </ContainerMap>
       </div> 
     </>
   );
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,46 @@
+import { useState } from "react";
 import styles from "./Card.module.css";
-import imgBotaoEditar from "../../assets/lapis.png";
+import trashIcon from "../../assets/trash.png";
 
-function Card({children}){
-    return(
-        <>
-            <div className={styles.cardCoordenada}>
-                <div className={styles.medidas} id="longitude">
-                    <p className={styles.textoMedidas}>Longitude:</p>
-                    <p className={styles.textoMedidas} id="valorLongitude">50.0</p>
-                </div>
-                <div className={styles.medidas} id="latitude">
-                    <p className={styles.textoMedidas}>Latitude:</p>
-                    <p className={styles.textoMedidas} id="valorLatitude">60.0</p>
-                </div>
-                <div className={styles.areaBotaoEditar}>
-                    <button className={styles.botaoEditar} onclick="editarCoordenadas()">
-                        <img className={styles.imgBotaoEditar} src={imgBotaoEditar} alt="Botão editar, imagem de lápis" />
-                    </button>
-                </div>
-            </div>
-        </>
-    );
+
+function Card({ id, onRemove }) {
+  const [latitude, setLatitude] = useState("0.0");
+  const [longitude, setLongitude] = useState("0.0");
+
+  return (
+    <>
+      <div className={styles.cardCoordenada}>
+        <div className={styles.medidas} id="longitude">
+          <p className={styles.textoMedidas}>Longitude:</p>
+          <input
+            type="text"
+            className={styles.inputMedidas}
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+        </div>
+        <div className={styles.medidas} id="latitude">
+          <p className={styles.textoMedidas}>Latitude:</p>
+          <input
+            type="text"
+            className={styles.inputMedidas}
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+        </div>
+        <div className={styles.areaBotaoEditar}>
+          <button className={styles.botaoEditar} onClick={() => onRemove(id)}>
+            <img
+              className={styles.trashIcon}
+              src={trashIcon}
+              alt="Botão editar, imagem de lápis"
+            />
+          </button>
+        </div>
+        
+      </div>
+    </>
+  );
 }
 
 export default Card;
